ef044ef358856c24e7842ca7703a6327cc29eb3b
Kunal-Chhabra <105283137+Kunal-Chhabra@users.noreply.github.com>
2022-05-15 22:55:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-// callback
+
 var a=document.getElementById("submit_div")
 function greeting(name) {
 
   }
-  
+function submitingToAPI(){
+  return new Promise((res)=>{
+    setTimeout(res,3000);
+  });
+}
 
 function load(){
     var a=document.getElementById("submit_div")
